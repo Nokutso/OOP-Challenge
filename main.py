c1172d9b4cd34722eb7cc73f523ee14b41cbb5d1
Nokutso <shxronjack@gmail.com>
2025-04-19 05:50:48 +0200

    adding random feature

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 #IMPORTING PET CLASS
 import pet as p
+import random
+
+#RANDOMLY ASSIGNING HUNGER, ENERGY AND HAPPINESS LEVELS TO PET
+hunger = random.randint(1, 10)
+energy = random.randint(1, 10)
+happiness = random.randint(1, 10)
+
+
+#MAKING SURE THE OUTPUT LOOKS NICE ON THE OUTSIDE
+print("*******************************************")
+print("                My Pet")
+print("*******************************************")
+print(" ")
 
 #CREATING A PET
 #1. GIVING IT A NAME
 petName = input("Creating Pet: ")
-myPet = p.Pet(petName, 10, 10, 10)
+myPet = p.Pet(petName, hunger, energy, happiness)
 
 #2. GIVING IT TRICKS
-answer = input ("Teach your pet some tricks! Type 'done' when you're finished ")
+print("Teach your pet some tricks! Type 'done' when you're finished ")
 
 while True:
     trick = input("Enter a trick: ")
